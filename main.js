fc611b9bf85f36f34745db74787ef8e99e97e246
Nakaishi Ryosuke <acna2676@gmail.com>
2021-11-10 17:09:55 +0900

    create: ajax to apig

--- main.js
+++ main.js
@@ -8,3 +8,18 @@ $(function () {
     reader.readAsDataURL(event.target.files[0]);
   });
 });
+
+$(function () {
+  $.ajax({
+    type: "GET",
+    url: "https://5l5r3mxozc.execute-api.ap-northeast-1.amazonaws.com/dev/nike_apig",
+    dataType: "json",
+  }).then(
+    // 取得成功時
+    function (json) {},
+    function () {
+      // エラー発生時
+      alert("エラー時に表示されるテキスト");
+    }
+  );
+});
