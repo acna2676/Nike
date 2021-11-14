4362a5fdd8449f879ee2b623b80767d714875555
Nakaishi Ryosuke <acna2676@gmail.com>
2021-11-14 15:57:39 +0900

    note: tmp
    mend

--- main.js
+++ main.js
@@ -9,6 +9,36 @@ $(function () {
   });
 });
 
+// タスクを取得する関数
+$(function () {
+  $("#submit-task").on("click", function (event) {
+    let user_name = "acna2676";
+
+    $.ajax({
+      type: "GET",
+      url:
+        "https://gm0yznl72d.execute-api.ap-northeast-1.amazonaws.com/dev/resource?user_name=" +
+        user_name,
+      dataType: "json",
+    }).then(
+      // 取得成功時
+      function (json) {
+        let tasks = JSON.parse(json)["tasks"];
+        for (task in tasks) {
+          $(".tasks").append(
+            '<input type="checkbox" name="color" value="' + task + '" />' + task
+          );
+        }
+      },
+      function () {
+        // エラー発生時
+        alert("エラー時に表示されるテキスト");
+      }
+    );
+  });
+});
+
+// ボタンを押したらタスクを登録する関数
 $(function () {
   $("#submit-task").on("click", function (event) {
     let task = $("#task").val();
