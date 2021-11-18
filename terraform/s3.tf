4694d094e10ed7265582baf7b20c7c29549aca5b
Nakaishi Ryosuke <acna2676@gmail.com>
2021-11-18 11:08:48 +0900

    create s3 hosting template of terraform
    mend

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,49 @@
+resource "aws_cloudfront_distribution" "static-www-nike-dev" {
+    origin {
+        domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+        origin_id = aws_s3_bucket.bucket.id
+        s3_origin_config {
+          origin_access_identity = aws_cloudfront_origin_access_identity.static-www-nike-dev.cloudfront_access_identity_path
+        }
+    }
+
+    enabled =  true
+
+    default_root_object = "index.html"
+
+    default_cache_behavior {
+        allowed_methods = [ "GET", "HEAD" ]
+        cached_methods = [ "GET", "HEAD" ]
+        target_origin_id = aws_s3_bucket.bucket.id
+        
+        forwarded_values {
+            query_string = false
+
+            cookies {
+              forward = "none"
+            }
+        }
+
+        viewer_protocol_policy = "redirect-to-https"
+        min_ttl = 0
+        default_ttl = 3600
+        max_ttl = 86400
+    }
+
+    restrictions {
+      geo_restriction {
+          restriction_type = "whitelist"
+          locations = [ "JP" ]
+      }
+    }
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+}
+
+resource "aws_cloudfront_origin_access_identity" "static-www-nike-dev" {}
+
+
 resource "aws_s3_bucket" "bucket" {
   bucket_prefix = "static-www-nike-dev"
   acl = "private"
@@ -5,7 +51,7 @@ resource "aws_s3_bucket" "bucket" {
   website {
     index_document = "index.html"
     error_document = "error.html"
-  }     
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket" {
@@ -34,7 +80,9 @@ data "aws_iam_policy_document" "static-www-nike-dev" {
 resource "aws_s3_bucket_object" "index_page" {
   bucket = aws_s3_bucket.bucket.id
   key = "index.html"
-  source = "www/index.html"
+  source = "main.html"
   content_type = "text/html"
-  etag = filemd5("www/index.html")
+  etag = filemd5("main.html")
 }
+
+
